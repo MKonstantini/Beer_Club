110665e607935d1a53b32d84111365b0c0ba9223
Matan Konstantini <matanko35@gmail.com>
2024-06-11 21:02:18 +0300

    updates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,24 +6,34 @@ import { MenuIcon } from 'lucide-react'
 
 const Navbar = () => {
     return (
-        <nav className="fixed top-0 w-full bg-white">
-            <div className="p-5 flex border border-black justify-between items-center">
+        <nav className="fixed top-0 w-full bg-white border border-black">
+            <div className="md:max-w-screen-2xl w-full mx-auto p-5 flex justify-between items-center">
                 {/* Logo */}
-                <div className="flex items-center gap-5 w-52">
-                    <Image src={'BeerClub_Logo.svg'} width={50} height={50} alt="logo" />
-                    <p className="font-bold text-lg hidden sm:block">BEER CLUB</p>
-                </div>
+                <Button variant={'none'} asChild className="flex items-center gap-5 w-52">
+                    <Link href={'/'}>
+                        <Image src={'BeerClub_Logo.svg'} width={50} height={50} alt="logo" />
+                        <p className="font-bold text-lg hidden sm:block">BEER CLUB</p>
+                    </Link>
+                </Button>
 
                 {/* Middle Buttons */}
                 <div className="hidden lg:block">
-                    <Button variant={'black'} size={'lg'} className="mx-5">HOME</Button>
-                    <Button variant={'black'} size={'lg'} className="mx-5">DASHBOARD</Button>
-                    <Button variant={'black'} size={'lg'} className="mx-5">SHOP</Button>
+                    <Button variant={'black'} size={'lg'} asChild className="mx-5">
+                        <Link href={'/'}>HOME</Link>
+                    </Button>
+                    <Button variant={'black'} size={'lg'} asChild className="mx-5">
+                        <Link href={'/dashboard'}>DASHBOARD</Link>
+                    </Button>
+                    <Button variant={'black'} size={'lg'} asChild className="mx-5">
+                        <Link href={'/shop'}>SHOP</Link>
+                    </Button>
                 </div>
 
                 {/* Left Buttons */}
                 <div className="flex items-center justify-end w-52">
-                    <Button variant={'black'} size={'lg'}>LOGIN</Button>
+                    <Button variant={'black'} size={'lg'} asChild>
+                        <Link href={'/login'}>LOGIN</Link>
+                    </Button>
                     <Button variant={'none'} className="lg:hidden">
                         <MenuIcon className="ms-5" onClick={() => document.getElementById('mobile-menu')?.classList.toggle('hidden')} />
                     </Button>
@@ -33,9 +43,15 @@ const Navbar = () => {
             <div id="mobile-menu" className="hidden">
                 {/* Mobile Menu */}
                 <div className="flex flex-col lg:hidden">
-                    <Button variant={'black'} className="w-100 h-24">HOME</Button>
-                    <Button variant={'black'} className="w-100 h-24">DASHBOARD</Button>
-                    <Button variant={'black'} className="w-100 h-24">SHOP</Button>
+                    <Button variant={'black'} asChild className="w-100 h-24">
+                        <Link href={'/'}>HOME</Link>
+                    </Button>
+                    <Button variant={'black'} asChild className="w-100 h-24">
+                        <Link href={'/dashboard'}>DASHBOARD</Link>
+                    </Button>
+                    <Button variant={'black'} asChild className="w-100 h-24">
+                        <Link href={'/shop'}>SHOP</Link>
+                    </Button>
                 </div>
             </div>
         </nav>
