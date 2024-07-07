7f74deab895054ed9eb8ef1b3850ff65bb49f014
Matan Konstantini <matanko35@gmail.com>
2024-07-07 23:06:31 +0300

    updates

--- src/services/fetch-friends.ts
+++ src/services/fetch-friends.ts
@@ -1,16 +1,42 @@
 "use server";
 import { db } from "@/lib/db";
-import { Product } from "@prisma/client";
-import { Decimal } from "@prisma/client/runtime/library";
 
-// Create a friendship
-export const createFriendship = async (userId: number, friendId: number) => {
+// GET
+export const getAllFriendshipsForUser = async (userId: string) => {
+  try {
+    return await db.friendship.findMany({
+      where: {
+        OR: [{ userId }, { friendId: userId }],
+      },
+      include: {
+        user: true,
+        friend: true,
+      },
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+// CREATE
+export const createFriendship = async (userId: string, friendId: string) => {
   try {
     return await db.friendship.create({
       data: {
         userId,
         friendId,
       },
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+// DELETE
+export const deleteFriendship = async (id: string) => {
+  try {
+    return await db.friendship.delete({
+      where: { id },
     });
   } catch (err) {
     throw err;
--- src/services/fetch-orders.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-import { db } from "@/lib/db";
-import { Order } from "@prisma/client";
-import { Decimal } from "@prisma/client/runtime/library";
-
-export const calculateStoreCredit = (order: Order) => {
-  return cartTotal;
-};
